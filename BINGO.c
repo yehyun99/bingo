03f5a7b3d537ffca99afb8a1be83c7e79adcc99f
이예현 <shszyh@iclooud.com>
2018-11-11 19:40:23 +0900

    bingo

---
+++ BINGO.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int N,M;
+	printf("빙고테이블의 크기를 입력하시오.: ");
+	scanf("%d",&N);
+	printf("몇개의 줄로 승부를 낼지 정하시오.:");
+	scanf("%d",&M);
+	
+	
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
