e45bca8af50a2574b530144d959630b97f642b8e
이예현 <shszyh@iclooud.com>
2018-11-14 01:23:25 +0900

    bingo...error

--- BINGO.c
+++ BINGO.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -9,6 +10,14 @@ int main(int argc, char *argv[]) {
 	scanf("%d",&N);
 	printf("몇개의 줄로 승부를 낼지 정하시오.:");
 	scanf("%d",&M);
+	int i;
+		for (i=0;i<=N;i++){
+			for (i=0;i<=N;i++)
+		};
+	int initiate_bingo(int )
+	
+	
+	
 	
 	
 	 
